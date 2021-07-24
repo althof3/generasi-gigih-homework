1a5e29a7104f4c87274bf7182da0e2f776b74233
Althof ramdhan <althoframdhan3@gmail.com>
2021-07-24 14:33:36 +0700

    refactor: using react portal for modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,11 +1,21 @@
 import style from "./style.module.css";
+import ReactDOM from "react-dom";
+import { useEffect } from "react";
 
 const Modal = ({ onClose, children }) => {
-  return (
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
+  return ReactDOM.createPortal(
     <>
       <div className={style.modal} onClick={onClose} />
       <div className={style.modal__content}>{children}</div>
-    </>
+    </>,
+    document.getElementById("portal")
   );
 };
 
